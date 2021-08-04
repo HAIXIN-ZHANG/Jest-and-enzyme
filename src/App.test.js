1ab269c3861284a17b06fb984f9a0a30676d4685
haixin zhang <haixin.zhang.777@gmail.com>
2021-08-04 20:56:58 +1000

    set up env

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
+import React from "react";
 import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-17-updated";
 
-test("renders without err", () => {});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without err", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+});
